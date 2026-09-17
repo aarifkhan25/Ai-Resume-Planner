b013ef4545b56efec0882606e5a808468020452d
Aarif Khan <aarif69khan@gmail.com>
2026-09-17 19:13:31 +0530

    link upaded

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api=axios.create({
-    baseURL:"http://localhost:8888/api/auth",
+    baseURL:"https://ai-resume-planner.vercel.app/api/auth",
     withCredentials:true
     
 })
--- frontend/src/features/interview/services/interview.api.js
+++ frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api=axios.create({
-    baseURL:"http://localhost:8888/api/interview",
+    baseURL:"https://ai-resume-planner.vercel.app/api/interview",
     withCredentials:true
     
 })
