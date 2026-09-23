32b09a5ec95a7dbc3918a99d8108b07db1451203
Aarif Khan <165253484+aarifkhan25@users.noreply.github.com>
2026-09-23 17:37:59 +0530

    Update CORS origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const cookieParser=require("cookie-parser")
 app.use(express.json());    
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-resume-planner-zmha.vercel.app",
     credentials: true
 }))
 app.use("/api/auth",authRouter)
